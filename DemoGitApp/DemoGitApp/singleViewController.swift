00b4c7373d1d3c6c01ce9f25a792e5931b54c72e
V kishore kumar redddy <kishoreRedddy@inpuhjlp07999.local>
2020-06-17 14:39:24 +0530

    New controller added

---
+++ DemoGitApp/DemoGitApp/singleViewController.swift
@@ -0,0 +1,30 @@
+//
+//  singleViewController.swift
+//  DemoGitApp
+//
+//  Created by V kishore kumar redddy on 17/06/20.
+//  Copyright © 2020 V kishore kumar redddy. All rights reserved.
+//
+
+import UIKit
+
+class singleViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
