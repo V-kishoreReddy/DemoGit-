a93e6765741279a184d52bfce4cb6cf3a2d00ed6
V-kishoreReddy <67044489+V-kishoreReddy@users.noreply.github.com>
2020-06-17 14:28:45 +0530

    Add files via upload

---
+++ DemoGitApp/DemoGitApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DemoGitApp
+//
+//  Created by V kishore kumar redddy on 17/06/20.
+//  Copyright © 2020 V kishore kumar redddy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ DemoGitApp/DemoGitAppTests/DemoGitAppTests.swift
@@ -0,0 +1,34 @@
+//
+//  DemoGitAppTests.swift
+//  DemoGitAppTests
+//
+//  Created by V kishore kumar redddy on 17/06/20.
+//  Copyright © 2020 V kishore kumar redddy. All rights reserved.
+//
+
+import XCTest
+@testable import DemoGitApp
+
+class DemoGitAppTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
